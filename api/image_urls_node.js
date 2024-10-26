07a1bb242fb311bc3ab29ed3c0209becba1da545
Pradeep_siva <Pradeepsvpro@outlook.com>
2024-10-26 19:02:35 +0200

    Added b2 authorize and logs.

--- api/image_urls_node.js
+++ api/image_urls_node.js
@@ -7,20 +7,31 @@ module.exports = async (req, res) => {
     });
 
     try {
+        console.log('Authorizing B2 client...');
+        await b2.authorize();
+        console.log('B2 client authorized successfully.');
+
+        console.log('Listing files from bucket 1...');
         const bucket1Files = await b2.listFileNames({
             bucketId: 'Enagement-Pics1',
             maxFileCount: 316
         });
-
+        console.log('Listing files from bucket 2...');
         const bucket2Files = await b2.listFileNames({
             bucketId: 'Enagement-Pics2',
             maxFileCount: 447
         });
 
+
+        // Map file URLs
+        console.log('Mapping file URLs...');
         const imageUrls = [
             ...bucket1Files.data.files.map(file => b2.getDownloadUrl({ fileId: file.fileId })),
             ...bucket2Files.data.files.map(file => b2.getDownloadUrl({ fileId: file.fileId }))
         ];
+
+        // Return the response
+        console.log('Returning the image URLs.');
 
         res.status(200).json({ imageUrls });
     } catch (error) {
